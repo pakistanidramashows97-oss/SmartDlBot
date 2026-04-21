0a0e37125f2efb9dc73807cc845ca67d84340f83
anujedits42-byte <anujedits42@gmail.com>
2026-04-21 13:26:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ app = Client(
     "bot",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    in_memory=True  # ✅ FloodWait fix
+    bot_token=BOT_TOKEN 
 )
 
 # Setup handlers
